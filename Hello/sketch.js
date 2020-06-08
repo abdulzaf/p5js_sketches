b1f3ae176cef707660907fcba69af40586ce9d37
Abdullah Zafar <zafar.abdullah10@gmail.com>
2020-06-07 20:19:43 -0400

    Update sketch.js

--- Hello/sketch.js
+++ Hello/sketch.js
@@ -1,132 +1,176 @@
-/*
-* @name Drawing
-* @description Generative painting program.
-*/
-
-// All the paths
-let paths = [];
-// Are we painting?
-let painting = false;
-// How long until the next circle
-let next = 0;
-// Where are we now and where were we?
-let current;
-let previous;
+ArrayList <Triangle> triangles = new ArrayList<Triangle>();
+var dividing = false;
+var zooming = false;
+var divideStep = 0;
+var zoomStep = 0;
+var strokeSize = 4;
+var iterX = 0;
+var iterY = 0;
+var colFill = false;
 
 function setup() {
-  createCanvas(720, 400);
-  current = createVector(0,0);
-  previous = createVector(0,0);
-};
+  createCanvas(300, 300);
+  background(255);
+  fill(255);
+  noStroke();
+  makeWheel();
+  redrawAll(triangles);
+}
 
 function draw() {
-  background(200);
-  
-  // If it's time for a new point
-  if (millis() > next && painting) {
+  iterX = round((var) mouseX/width);
+  iterY = round((var) mouseY/height);
 
-    // Grab mouse position      
-    current.x = mouseX;
-    current.y = mouseY;
+  var triangleSize = dist(triangles.get(0).vert[0].x, triangles.get(0).vert[0].y, triangles.get(0).vert[1].x, triangles.get(0).vert[1].y);
+  strokeSize = triangleSize/50;
+  var ds = 0.8*exp(-0.009*triangleSize);
 
-    // New particle's force is based on mouse movement
-    let force = p5.Vector.sub(current, previous);
-    force.mult(0.05);
+    ArrayList<Triangle> temp = new ArrayList<Triangle>();
 
-    // Add new particle
-    paths[paths.length - 1].add(current, force);
-    
-    // Schedule next circle
-    next = millis() + random(100);
-
-    // Store mouse values
-    previous.x = current.x;
-    previous.y = current.y;
-  }
-
-  // Draw all paths
-  for( let i = 0; i < paths.length; i++) {
-    paths[i].update();
-    paths[i].display();
-  }
-}
-
-// Start it up
-function mousePressed() {
-  next = 0;
-  painting = true;
-  previous.x = mouseX;
-  previous.y = mouseY;
-  paths.push(new Path());
-}
-
-// Stop
-function mouseReleased() {
-  painting = false;
-}
-
-// A Path is a list of particles
-class Path {
-  constructor() {
-    this.particles = [];
-    this.hue = random(100);
-  }
-
-  add(position, force) {
-    // Add a new particle with a position, force, and hue
-    this.particles.push(new Particle(position, force, this.hue));
-  }
-  
-  // Display plath
-  update() {  
-    for (let i = 0; i < this.particles.length; i++) {
-      this.particles[i].update();
-    }
-  }  
-  
-  // Display plath
-  display() {    
-    // Loop through backwards
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      // If we shold remove it
-      if (this.particles[i].lifespan <= 0) {
-        this.particles.splice(i, 1);
-      // Otherwise, display it
+    if (dividing) {
+      if (divideStep < 1) {
+        temp = subdivide(triangles, divideStep);
+        redrawAll(temp);
+        divideStep += ds;
       } else {
-        this.particles[i].display(this.particles[i+1]);
+        triangles = subdivide(triangles, 1);
+        dividing = false;
+        redrawAll(triangles);
       }
     }
-  
-  }  
-}
 
-// Particles along the path
-class Particle {
-  constructor(position, force, hue) {
-    this.position = createVector(position.x, position.y);
-    this.velocity = createVector(force.x, force.y);
-    this.drag = 0.95;
-    this.lifespan = 255;
-  }
-
-  update() {
-    // Move it
-    this.position.add(this.velocity);
-    // Slow it down
-    this.velocity.mult(this.drag);
-    // Fade it out
-    this.lifespan--;
-  }
-
-  // Draw particle and connect it with a line
-  // Draw a line to another
-  display(other) {
-    stroke(0, this.lifespan);
-    fill(0, this.lifespan/2);    
-    ellipse(this.position.x,this.position.y, 8, 8);    
-    // If we need to draw a line
-    if (other) {
-      line(this.position.x, this.position.y, other.position.x, other.position.y);
+  if (zooming) {
+    if (zoomStep < 10) {
+      triangles = zoom(1.2);
+      redrawAll(triangles);
+      zoomStep++;
+    } else {
+      zooming = false;
+      zoomStep = 0;
     }
   }
+    if (iterX==0 && iterY==0) {
+      colFill = true;
+      redrawAll(triangles);
+    } else if (iterX==1 && iterY==1) {
+      colFill = false;
+      redrawAll(triangles);
+    }
+}
+
+function mouseClicked() {
+    var triangleSize = dist(triangles.get(0).vert[0].x, triangles.get(0).vert[0].y, triangles.get(0).vert[1].x, triangles.get(0).vert[1].y);
+    if (iterX==1 && iterY==0 && triangles.createCanvas() < 500) {
+      dividing = true;
+      divideStep = 0;
+    }
+    if (!dividing && !zooming) {
+      if (iterX==0 && iterY==1 && triangleSize < width/4) {
+        zooming = true;
+      }
+    }
+}
+
+function makeWheel() {
+  var origin = new var(width/2, height/2);
+  var r = 0.5*width;
+  var num = 2;
+  var den = 5*num;
+  for (var i = 0; i < den; i++) {
+    var b = new var(origin.x + r*cos((num*i-1)*PI/den), origin.y + r*sin((num*i-1)*PI/den));
+    var c = new var(origin.x + r*cos((num*i+1)*PI/den), origin.y + r*sin((num*i+1)*PI/den));
+    if (i%2 == 0) { //mirror every second triangle
+      var temp = b;
+      b = c;
+      c = temp;
+    }
+    triangles.add(new Triangle(true, origin, b, c));
+  }
+}
+
+ArrayList<Triangle> subdivide(ArrayList<Triangle> tList, var s) {
+  ArrayList<Triangle> result = new ArrayList<Triangle>();
+  var phi = (1 + sqrt(5))/2;
+  for (var i = 0; i < tList.createCanvas (); i++) {
+    Triangle t = tList.get(i);
+    if (t.red) {
+      var p = new var(t.vert[0].x +s*(t.vert[1].x - t.vert[0].x)/phi, t.vert[0].y +s*(t.vert[1].y - t.vert[0].y)/phi);
+      addToList(result, new Triangle(true, t.vert[2], p, t.vert[1]));
+      addToList(result, new Triangle(false, p, t.vert[2], t.vert[0]));
+    } else {
+      var q = new var(t.vert[0].x - (1-1/phi)*s*(t.vert[0].x - t.vert[1].x), t.vert[0].y - (1-1/phi)*s*(t.vert[0].y - t.vert[1].y) );
+      var r = new var(t.vert[1].x + (t.vert[2].x - t.vert[1].x)/phi, t.vert[1].y + (t.vert[2].y - t.vert[1].y)/phi );
+      addToList(result, new Triangle(false, r, t.vert[2], t.vert[0]));
+      addToList(result, new Triangle(false, q, r, t.vert[1]));
+      addToList(result, new Triangle(true, r, q, t.vert[0]));
+    }
+  }
+  return result;
+}
+
+function redrawAll(ArrayList<Triangle> tList) {
+  background(255);
+  for (var i = 0; i < tList.createCanvas (); i++) {
+    tList.get(i).redraw();
+  }
+}
+
+function addToList(ArrayList<Triangle> tList, Triangle t) {
+  if ((t.vert[0].x >= 0 && t.vert[0].x <= width && t.vert[0].y >= 0 && t.vert[0].y <= height) ||
+    (t.vert[1].x >= 0 && t.vert[1].x <= width && t.vert[1].y >= 0 && t.vert[1].y <= height) ||
+    (t.vert[2].x >= 0 && t.vert[2].x <= width && t.vert[2].y >= 0 && t.vert[2].y <= height)) {
+    tList.add(t);
+  }
+}
+
+ArrayList<Triangle> zoom(var factor) {
+  var origin = new var(width/2, height/2);
+  ArrayList<Triangle> result = new ArrayList<Triangle>();
+  var[] v = new var[3];
+  for (var i = 0; i < triangles.createCanvas (); i++) {
+    for (var j = 0; j < 3; j++) {
+      var r = dist(origin.x, origin.y, triangles.get(i).vert[j].x, triangles.get(i).vert[j].y);
+      var angle = atan2(triangles.get(i).vert[j].y - origin.y, triangles.get(i).vert[j].x - origin.x);
+      v[j] = new var(origin.x + r*factor*cos(angle), origin.y + r*factor*sin(angle));
+    }
+    addToList(result, new Triangle(triangles.get(i).red, v[0], v[1], v[2])) ;
+  }
+  return result;
+}
+
+class Triangle {
+
+  var red;
+  var[] vert = new var[3];
+  color c1 = color(0);
+  color c2 = color(150*((var) mouseX/width), 0, 100*((var) mouseY/height));
+  color c3 = color(150*((var) mouseX/width), 0, 100*((var) mouseY/height));
+
+  Triangle(var red, var a, var b, var c) {
+    this.red = red;
+    this.vert[0] = a;
+    this.vert[1] = b;
+    this.vert[2] = c;
+  }
+
+  function redraw() {
+    if (red) {
+      if (colFill) {
+        fill(c2);
+      }
+      stroke(c2);
+    } else {
+      fill(255);
+      stroke(c1);
+    }
+    strokeWeight(1);
+    triangle(vert[1].x, vert[1].y, vert[0].x, vert[0].y, vert[2].x, vert[2].y);
+    stroke(c3);
+    strokeWeight(2);
+    line(vert[1].x, vert[1].y, vert[0].x, vert[0].y);
+    line(vert[0].x, vert[0].y, vert[2].x, vert[2].y);
+    noStroke();
+  }
+
+
 }
